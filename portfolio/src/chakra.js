35b3ca256adaeaed1ea60cf17f262103fcb7d393
AlexEl972 <alexendre.elie@epitech.eu>
2023-06-02 16:31:10 +0200

    ze

--- portfolio/src/chakra.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { ChakraProvider, CSSReset } from '@chakra-ui/react';
-
-export const Chakra = ({ children }) => (
-  <ChakraProvider>
-    <CSSReset />
-    {children}
-  </ChakraProvider>
-);
--- portfolio1/src/index.js
+++ portfolio1/src/index.js
@@ -2,20 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import theme from "./theme";
-import { ColorModeScript } from "@chakra-ui/react";
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from "@chakra-ui/react";
-import { Chakra } from './chakra';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider theme={theme}>
-    <React.StrictMode>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+  <React.StrictMode>
     <App />
-    </React.StrictMode>
-  </ChakraProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
